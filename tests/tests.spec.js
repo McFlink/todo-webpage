c8bd4c3a1d3e815d8176c9c4cdfaaf3eb28735c0
Axel Calås <axel.calas.semst@gmail.com>
2024-02-14 11:08:18 +0100

    Test 1 complete, test 2 and 3 needs the website to be more complete.

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,19 +1,48 @@
-// @ts-check
 const { test, expect } = require('@playwright/test');
+const { text } = require('stream/consumers');
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('localhost:5501');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('add one item, confirm it has been created', async ({ page }) => {
+  const todoItem = page.locator("#todo-input");
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await todoItem.fill("Dammsuga");
+  await todoItem.press("Enter");
+  await expect(page.getByText("Dammsuga")).toBeVisible();
 });
+
+test('add one item, confirm "1 item left", check the item and confirm "0 items left"', async ({ page }) => {
+  const todoItem = page.locator("#todo-input");
+  const itemCount = page.locator("#item-count");
+
+  await todoItem.fill("Träna");
+  await todoItem.press("Enter");
+  
+  //Check if itemCount === "1 item left"
+  await expect(itemCount).toHaveText("1 item left");
+
+  //Complete task and check if itemCount === "0 items left"
+  //Need to build more on the program before testing this, currently we can't complete tasks. 
+
+});
+
+test('add 3 items, check one item and confirm "2 items left"', async ({ page }) => {
+  const todoItem = page.locator("#todo-input");
+
+  await todoItem.fill("Dammsuga");
+  await todoItem.press("Enter");
+
+  await todoItem.fill("Träna");
+  await todoItem.press("Enter");
+  
+  await todoItem.fill("Äta");
+  await todoItem.press("Enter");
+
+  //Complete one task and check if itemCount === "2 items left"
+  //Need to build more on the program before testing this, currently we can't complete tasks. 
+
+});
+
+
