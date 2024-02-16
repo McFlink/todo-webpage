fa6519888392a071e483a7b18b746d331136da39
Axel Calås <axel.calas.semst@gmail.com>
2024-02-16 10:05:26 +0100

    Finished all tests.

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,5 +1,4 @@
 const { test, expect } = require('@playwright/test');
-const { text } = require('stream/consumers');
 
 test.beforeEach(async ({ page }) => {
   await page.goto('localhost:5501');
@@ -24,12 +23,13 @@ test('add one item, confirm "1 item left", check the item and confirm "0 items l
   await expect(itemCount).toHaveText("1 item left");
 
   //Complete task and check if itemCount === "0 items left"
-  //Need to build more on the program before testing this, currently we can't complete tasks. 
-
+  await page.locator(".checkbox").click();
+  await expect(itemCount).toHaveText("0 items left");
 });
 
 test('add 3 items, check one item and confirm "2 items left"', async ({ page }) => {
   const todoItem = page.locator("#todo-input");
+  const itemCount = page.locator("#item-count");
 
   await todoItem.fill("Dammsuga");
   await todoItem.press("Enter");
@@ -41,8 +41,8 @@ test('add 3 items, check one item and confirm "2 items left"', async ({ page }) 
   await todoItem.press("Enter");
 
   //Complete one task and check if itemCount === "2 items left"
-  //Need to build more on the program before testing this, currently we can't complete tasks. 
-
+  await page.locator(".checkbox").first().click();
+  await expect(itemCount).toHaveText("2 items left");
 });
 
 
